c2ab1e0a941c69be69fa0f64d409d0cb26fe8749
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-07-25 15:21:13 -0300

    ✨ feat (login): criando função de login e atribuindo endpoint

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const usersRoutes = require('./users.routes')
+const LoginController = require('../controllers/LoginController')
 
 const routes = new Router()
 
 routes.use('/user', usersRoutes)
+routes.post('/login', LoginController.login)
 
 module.exports = routes
