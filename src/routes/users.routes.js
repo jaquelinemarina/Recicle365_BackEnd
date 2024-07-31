34ab32f22ef67f47cf173653b55168020ca49860
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-07-30 23:01:31 -0300

    ✨ feat (swagger): adicionando documentação para a rota de usuários

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,25 @@ const UserController = require('../controllers/UserController')
 
 const usersRoutes = new Router()
 
-usersRoutes.post("/", UserController.create)
+usersRoutes.post("/", UserController.create
+    /*
+    #swagger.tags = ['Usuários'],
+    #swagger.description = 'Endpoint para cadastrar um novo usuário.',
+    #swagger.parameters['createUser'] = {
+        in: 'body',
+        description: 'Informações do usuário.',
+        required: true,
+        schema: { 
+            $name: "Zé Ninguém",
+            $cpf: "12345678901",
+            $sex: "feminino", 
+            $birth: "1990-01-01",
+            $adress: "Rua dos Bobos, nº 0",
+            $email: "email@example.com",
+            $password: "12345Jj$"
+        }
+    }
+    */
+)
 
 module.exports = usersRoutes
